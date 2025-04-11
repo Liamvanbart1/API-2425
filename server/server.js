00dab2cb328c86320f0c0b0006b407a6afbf4fb6
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2025-04-11 13:14:11 +0200

    images

--- server/server.js
+++ server/server.js
@@ -38,35 +38,6 @@ app
   .use("/", sirv("dist"))
   .listen(3000, () => console.log("Server available on http://localhost:3000"));
 
-// app.get("/", async (req, res) => {
-//   const key = process.env.API_KEY;
-//   const baseUrl = process.env.BASE_URL;
-//   const leagueIds = [39, 140, 135, 78, 61];
-
-//   const endPoint = new URL("standings?league=39&season=2023", baseUrl);
-
-//   const response = await fetch(`${endPoint.href}`, {
-//     method: "GET",
-//     headers: {
-//       "x-apisports-key": key,
-//       "x-rapidapi-host": "v3.football.api-sports.io",
-//     },
-//   });
-//   const json = await response.json();
-//   if (json.errors.length) {
-//     console.log("fout met de api", json.errors);
-//   }
-//   console.log(json);
-//   const standings = json.response?.[0]?.league?.standings?.[0] || [];
-
-//   return res.send(
-//     renderTemplate("server/views/index.liquid", {
-//       title: "Standings",
-//       items: standings,
-//     })
-//   );
-// });
-
 app.get("/", async (req, res) => {
   const leagues = [
     { id: 39, name: "Premier League" },
@@ -100,9 +71,8 @@ app.get("/competitions/:id", async (req, res) => {
   });
 
   const json = await response.json();
-  if (json.errors && Object.keys(json.errors).length) {
-    console.log("Fout met de API", json.errors);
-    return res.status(500).send("API error");
+  if (json.errors.length) {
+    console.log("fout met de api", json.errors);
   }
 
   const leagueName = json.response?.[0]?.league?.name || `League ${id}`;
@@ -112,20 +82,6 @@ app.get("/competitions/:id", async (req, res) => {
     renderTemplate("server/views/competitions.liquid", {
       title: leagueName,
       items: standings,
-    })
-  );
-});
-
-app.get("/plant/:id/", async (req, res) => {
-  const id = req.params.id;
-  const item = data[id];
-  if (!item) {
-    return res.status(404).send("Not found");
-  }
-  return res.send(
-    renderTemplate("server/views/detail.liquid", {
-      title: `Detail page for ${id}`,
-      item,
     })
   );
 });
