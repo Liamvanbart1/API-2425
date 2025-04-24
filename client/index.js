c93e16b58f3d838abee6a0cd0fb615c92f44e7e9
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2025-04-24 17:53:47 +0200

    test

--- client/index.js
+++ client/index.js
@@ -1,6 +1,4 @@
 import "./index.css";
-import "../server/views/competitions.css";
-import "../server/views/results.css";
 
 const competitionDiv = document.querySelector(".competitions");
 const button = document.getElementById("toggleButton");
